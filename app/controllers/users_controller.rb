fc38234d67191aa82ced084cefd1346e93f7662b
Ghizmilth <hidanerferrer@gmail.com>
2017-08-26 16:15:36 -0700

    added login form, and email-password authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,25 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def create
+    @user = User.create(user_params)
+    redirect_to root_path
+  end
+
   def new
     #we make a new users
     # to pass to the form vieaw later
     @user = User.new
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 
 end
