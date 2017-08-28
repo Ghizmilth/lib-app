dc7169c30c2ab99dfc00e8ed31977588b47a24db
Ghizmilth <hidanerferrer@gmail.com>
2017-08-28 09:41:50 -0700

    added library controllers, routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'users#index'
-
+ #User routes
   get '/users/new', to: 'users#new', as: 'new_user'
   post '/users', to: 'users#create'
   get '/users/:id', to: 'users#show', as: 'user'
 
+#Auth routes
   get '/login', to: 'sessions#new'
   get '/logout', to: 'sessions#destroy'
   post '/sessions', to: 'sessions#create'
 
+#library rouytes
+  get '/libraries', to: 'libraries#index'
+  get '/libraries/new', to: 'libraries#new', as: 'new_library'
+  post '/libraries', to: 'libraries#create'
+  
 end
